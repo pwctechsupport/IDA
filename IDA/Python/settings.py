9451db6079eb3ae9412f7814bbdfe02cd2172a1d
Victor Wijaya <victor.w.wijaya@pwc.com>
2023-12-08 11:02:39 +0700

    connect to subapase

--- IDA/Python/settings.py
+++ IDA/Python/settings.py
@@ -27,8 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2rm615fe=*#1ji*5e$fd$b@cpzmdjtzx-g4fsy(n(^=y7u_mii'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = False
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'ida-production.up.railway.app']
 
@@ -87,12 +86,12 @@ WSGI_APPLICATION = 'Python.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': 'db.sqlite3',
+#     }
+# }
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'mssql',
@@ -103,16 +102,16 @@ DATABASES = {
 #         }
 #     }
 # }
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': 'IDA',
-#        'USER': 'postgres',
-#        'PASSWORD': 'Jakarta2022!',
-#        'HOST': 'localhost',
-#        'PORT': '5432',
-#    }
-# }
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'postgres',
+       'USER': 'postgres',
+       'PASSWORD': 'postgres_Jakarta1!',
+       'HOST': 'db.fnrmnbkqrzzdnbfxmzpl.supabase.co',
+       'PORT': '5432',
+   }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
